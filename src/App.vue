85889dcaa9ae43c57481d0fa1dc166eba9815b6e
Manoj Naidu <manojnaidu619@gmail.com>
2019-09-21 22:29:24 +0530

    First commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Index></Index>
+    <Login></Login>
+  </div>
+</template>
+
+<script>
+import Index from './components/Index.vue'
+import Login from './components/Login.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Index,
+    Login
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
