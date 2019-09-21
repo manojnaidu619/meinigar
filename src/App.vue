49b15ec6b46a5293429f8cbca7966f1caad182bf
Manoj Naidu <manojnaidu619@gmail.com>
2019-09-21 22:39:38 +0530

    Added vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <Index></Index>
-    <Login></Login>
+    <router-view></router-view>
   </div>
 </template>
 
---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import Index from './components/Index.vue'
+import Login from './components/Login.vue'
+
+export const routes = [
+  {path: '', component: Index},
+  {path: '/login', component: Login}
+]
